dd681b955a5f1b5670d65459448b5b335faa7b85
Murtaza Raza <rmm95110@gmail.com>
2023-09-20 23:39:25 +0400

    Added base classes for singleton. Added UiManager skeleton.

---
+++ Assets/Scripts/Base/UnitySingleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Utils.BaseClasses
+{
+    /// <summary>
+    /// </summary>
+    /// <typeparam name="T">Pass the name of this class so a proper typed static instance is created</typeparam>
+    public class UnitySingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        var obj = new GameObject();
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Base/UnitySingletonPersistant.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Utils.BaseClasses
+{
+	/// <summary>
+	/// Persistent Version of the Singleton Class.V1.0
+	/// </summary>
+	/// <typeparam name="T">Pass the name of this class so a proper typed static instance is created</typeparam>
+	public class UnitySingletonPersistent<T> : MonoBehaviour where T : Component
+	{
+		private static T instance;
+
+		public static T Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					instance = FindObjectOfType<T>();
+					if (instance == null)
+					{
+						var obj = new GameObject {hideFlags = HideFlags.HideAndDontSave};
+						instance = obj.AddComponent<T>();
+					}
+				}
+
+				return instance;
+			}
+		}
+
+		public virtual void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+			if (instance == null)
+			{
+				instance = this as T;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
