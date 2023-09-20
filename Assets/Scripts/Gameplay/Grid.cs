39f215729d58cf6eff1ed62ce7bf12732ad7f827
Murtaza Raza <rmm95110@gmail.com>
2023-09-20 23:41:27 +0400

    Added Async sprite loader script.

--- Assets/Scripts/Gameplay/Grid.cs
+++ Assets/Scripts/Gameplay/Grid.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void InitializeGrid(int x, int y)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // TODO add deserialized parameters to initialize grid from saved values
+    public void ReInitializeGrid()
     {
-        
+
+        throw new NotImplementedException();
     }
 }
--- Assets/Scripts/Utils/AssetLoaderUtil.cs
+++ Assets/Scripts/Utils/AssetLoaderUtil.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.UI;
 
-public class AssetLoaderUtil : MonoBehaviour
+namespace Utils
 {
-    // Start is called before the first frame update
-    void Start()
+    public class AssetLoaderUtil
     {
-        
-    }
+        public static async Task<Sprite> LoadSpriteAsync(AssetReferenceSprite spriteReference)
+        {
+            AsyncOperationHandle<Sprite> handle = spriteReference.LoadAssetAsync();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            await handle.Task;
+
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                return handle.Result;
+            }
+            else
+            {
+                Debug.LogError("Failed to load sprite: " + handle.OperationException);
+                return null;
+            }
+        }
     }
 }
